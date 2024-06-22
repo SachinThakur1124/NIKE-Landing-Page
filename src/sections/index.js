0078ddef1584a4d78cb559664a1bcc2bd72da8b8
SachinThakur1124 <Sachinthakur7814@gmail.com>
2024-06-22 16:54:10 +0530

    First Commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import Services from './Services';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import SpecialOffers from './SpecialOffers';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+
+
+export {
+    Hero,
+    Services,
+    Footer,
+    PopularProducts,
+    SpecialOffers,
+    SuperQuality,
+    Subscribe,
+    CustomerReviews,
+}
